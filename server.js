8dcca2fc87708f741a83f6ac174c1d1c727a87dc
shennhuang <shen.huang@104.com.tw>
2018-05-31 14:48:18 +0800

    delete post

--- server.js
+++ server.js
@@ -29,42 +29,6 @@ app.post("/sample", function (req, res) {
 
 
 function doPost(msg) {
-    console.log(msg)
-    // Line
-    let CHANNEL_ACCESS_TOKEN = 'TvVIuHAxbM/g/+Oh8gfXoWlUZPB2L3BvMMcdBUOajs3juaWxCOQR3q1hakYhRWHmgMofj34PKZW+o6PL8GIwrNsKPgf857LTnLHsN75DqZAqiymqI6Vyyo73rqVr0NDJZYEVrmpSLIZl1ztxOh4pGwdB04t89/1O/w1cDnyilFU=';
-    let PRINT_ACCESS_TOKEN = "ya29.c.El-_BcUqZr-01xARBjQexEMih8eYR03C9g7nMQBCFvr5vbrTDdurpePjICqNU1gf5epvlPGvBpnA1ywM4bT53dXcTNsm5AxkTo43PXC0HM4a-vOnmu2dvRw6ATXzngxM2g"
-    let text = msg.events[0].message.text
-    // 取出 replayToken 和發送的訊息文字
-    let replyToken = msg.events[0].replyToken;
-    let userMessage = msg.events[0].message.text;
-
-    if (typeof replyToken === 'undefined') {
-        return;
-    }
-
-    let url = 'https://translation.googleapis.com/language/translate/v2'
-    let options = {
-        method: 'POST',
-        url,
-        json: true,
-        headers: {
-            'content-Type': 'application/x-www-form-urlencoded',
-            'authorization': "Bearer " + PRINT_ACCESS_TOKEN
-        },
-        form: {
-            'q': 'hi',
-            'source': 'en',
-            'target': 'zh',
-            'format': 'text'
-        }
-    }
-    request(options, function (error, response, body) {
-        if (error) {
-            console.log(error)
-            return
-        }
-        console.log(body)
-    })
 }
 
 function doSample() {
