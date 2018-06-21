bf2c739672049904f87604bcfd64a080c86640e3
shennhuang <shen.huang@104.com.tw>
2018-06-21 14:05:20 +0800

    final版 V0.0.0

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 
 let express = require('express');
 let app = express();
+
 let request = require('request')
+
+let config = require('./config.json')
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -11,57 +14,127 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
+// Imports the Google Cloud client library
+let Translate = require('@google-cloud/translate');
+
+// Instantiates a client
+let translate = new Translate({
+    projectId: config.projectId,
+    keyFilename: config.keyFilename,
+});
+
 app.get("/", function (req, res) {
     return res.send('is GET');
 })
 
-app.post("/", function (req, res) {
-    console.log('dopost')
-    doPost(req.body)
-    return;
-})
 
 app.post("/sample", function (req, res) {
     console.log('doSample')
-    doSample()
-    return;
+    doSample(req)
+    return res.sendStatus(200);
 })
 
-
-function doPost(msg) {
-}
-
-function doSample() {
-    // Imports the Google Cloud client library
-    const Translate = require('@google-cloud/translate');
-
-    // Your Google Cloud Platform project ID
-    const projectId = 'project-id-1957904988521210417';
-
-    const keyFilename = '/home/ec2-user/linebot/linebot-852705fba2b0.json';
-
-    // Instantiates a client
-    const translate = new Translate({
-        projectId: projectId,
-        keyFilename: keyFilename,
-    });
+function doSample(req) {
 
     // The text to translate
-    const text = 'Hello, world!';
-    // The target language
-    const target = 'zh';
+    const text = req.body.events[0].message.text;
+    detectLanguage(text, function(target){
+        let replyResult = '';
 
-    // Translates some text into Russian
+        let replyToken = req.body.events[0].replyToken
+        let options = {
+            method: 'POST',
+            uri: 'https://api.line.me/v2/bot/message/reply',
+
+            // 客制化 headers 內容，主要告訴 Line 收到的訊息格式和 Line Bot 的 Token
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': config.lineAuthorization
+            },
+            body: {
+                // 告訴 Line 這個訊息回覆給誰
+                replyToken: replyToken,
+                messages: [{ type: 'text', text: replyResult }],
+            },
+            json: true,
+        };
+        if (target) {
+            // 進行翻譯
+            translateLanguage(text, target[0], replyResult, function(replyResult){
+                translateLanguage(text, target[1], replyResult, function(replyResult){
+                    
+                    options.body.messages[0].text = replyResult
+                    request(options, function (error, response, body) {
+                        if (error) throw new Error(error);
+        
+                        // console.log(body);
+                    });
+                })
+            })
+        } else {
+            options.body.messages[0].text = '語言偵測錯誤'
+            request(options, function (error, response, body) {
+                if (error) throw new Error(error);
+
+                // console.log(body);
+            });
+        }
+    })
+
+    
+}
+function detectLanguage (text, callback){
+    // 中文: zh-tw 英文: en 日文: ja
+    let switchLanguage = ['zh-TW', 'en', 'ja'];
+    let target = switchLanguage;
+
     translate
-        .translate(text, target)
+        .detect(text)
         .then(results => {
-            const translation = results[0];
+            let detections = results[0];
+            detections = Array.isArray(detections) ? detections : [detections];
+            detections = detections[0].language
+            detections = detections.indexOf('zh') >= 0 ? 'zh-TW' : detections
 
-            console.log(`Text: ${text}`);
-            console.log(`Translation: ${translation}`);
+            verifyTarget = target.indexOf(detections) >= 0 ? target.splice(target.indexOf(detections), 1) : -1 ;
+            if (verifyTarget == -1) {
+                target = false;
+            }
+            callback(target)
         })
         .catch(err => {
             console.error('ERROR:', err);
         });
 }
-app.listen(80);
+function translateLanguage(text, target, replyResult, callback){
+    translate
+        .translate(text, target)
+        .then(results => {
+            translationText = results[0];
+
+            target = target.indexOf('zh') >= 0 ? 'zh-TW' : target;
+            targetLanguage = ''
+            
+            switch (target) {
+                case 'en' :
+                    targetLanguage = 'English:'
+                    break;
+                case 'ja' :
+                    targetLanguage = '日本語:'
+                    break;
+                case 'zh-TW':
+                    targetLanguage = '中文:'
+                    break;
+                default :
+                    targetLanguage = 'fail'
+            }
+            if (targetLanguage !== 'fail') {
+                replyResult += targetLanguage + "\n" + translationText + "\n\n"
+            }
+            
+
+            callback(replyResult)
+        })
+    
+}
+app.listen(3333);
